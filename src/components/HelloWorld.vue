7d54ac048a5d74d53732a418c6dd58679d481239
tanhuilong <1623517095@qq.com>
2020-03-27 21:52:15 +0800

    mingling

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+  <ul>
+    <li><a href="#"><img src="../../public/img/menu_icon_gzt.png"><span> 业绩管理</span><i class="el-icon-arrow-down
+ el-icon--right"></i></a>
+ <ul><li @click="menu1"><a href="#">业绩报表</a></li></ul>
+ </li>
+    <li><a href="#"><img src="../../public/img/menu_icon_yygl.png"> 运营管理<i class="el-icon-arrow-down
+ el-icon--right"></i></a></li>
+    <li><a href="#"><img src="../../public/img/menu_icon_xt.png"> 系统管理<i class="el-icon-arrow-down
+ el-icon--right"></i></a>
+ <ul>
+   <li><a href="#">用户管理</a></li>
+   <li @click="menu2"><a href="#">角色权限管理</a></li>
+ </ul>
+ </li>
+  </ul>
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    menu1(){
+      
+    },
+    menu2(){
+      
+    }
+  }
+}
+</script>
+
+
+<style scoped lang="less">
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+}
+a {
+  display: block;
+  width:170px;
+  color: #fff;
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+}
+.hello li ul{
+  display: none;
+}
+.hello li a:hover,.hello li ul li a:hover{
+  background: red;
+}
+.hello li:hover ul{
+  display: block;
+}
+</style>
---
+++ src/components/Liebiao.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="lie">
+        <div class="l-top">
+    <el-row :gutter="10">
+  <el-col :span="2"><div class="grid-content bg-purple">序号</div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple">地区公司</div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple">邀请注册</div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple">分享海报</div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple">客户浏览</div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple">已推荐</div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple">已到访</div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple">已认购</div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple">已签到</div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple">操作</div></el-col>
+</el-row>
+</div>
+<div class="l-mid" >
+    <el-row :gutter="10">
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.num}}</li></div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.place}}</li></div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.share}}</li></div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.sharesea}}</li></div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.scan}}</li></div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.tui}}</li></div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.fang}}</li></div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.gou}}</li></div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.qian}}</li></div></el-col>
+  <el-col :span="2"><div class="grid-content bg-purple"><li v-for="(item,index) in list" :key="index">{{item.op}}</li></div></el-col>
+</el-row>
+</div>
+<div class="l-footer">
+    <el-pagination
+  background
+  layout="prev, pager, next"
+  :total="1000">
+</el-pagination>
+</div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+       var _this=this;
+       var url="json/lie.json";
+       this.$http.get(url).then(res=>{
+           _this.list=res.data
+       })
+       return{
+           list:[]
+       }
+        
+    },
+    methods:{
+                   
+    }
+}
+</script>
+<style >
+.l-top{
+    height: 40px;
+    color: #fff;
+    margin:10px 0 0 10px;
+
+}
+.l-mid{
+    margin-left: 10px;
+}
+.el-col div {
+    font-size: 14px;
+}
+li{
+    list-style: none;
+    height: 30px;
+}
+.l-footer{
+   text-align: center;
+}
+</style>
